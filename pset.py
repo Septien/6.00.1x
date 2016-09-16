4168876a9975ab46baf24aba3fb9d045c32a6bfb
Jose Septien <jaseptienh@gmail.com>
2016-09-16 14:30:21 -0500

    Add solution to problem 2

--- pset.py
+++ pset.py
@@ -5,32 +5,45 @@ Created on Tue Sep  6 19:29:26 2016
 @author: Phantom
 
 Problem set 2.
-Problem 1.
-Write a program that calculates the credit car balance after one 
-year if a person only pays the minimum monthly payment required by 
-the credit card company.
+Problem 2.
+Write a program that calculates he minimum fixed monthly payment needed in 
+order pay off a credit card balance within 12 months. By a fixed monthly payment, 
+we mean a single number which does not change each month, but instead is a constant 
+amount that will be paid each month.
 """
-def ccBalance(balance, annualInterestRate, monthlyPaymentRate):
+
+def fpccBalance(balance, annualInterestRate):
     """
+    Calculates the minimum monthly payment that pay off the debt in one year
     balance: int or float. Indicate the oustanding balance of the credit card
     annualInterestRate: float. annual interes rate as decimal.
-    monthlyPaymentRate: minimum monthly payment rate as decimal.
     """
-    ub = balance                #unpaid balance
-    aIR = annualInterestRate
-    mPR = monthlyPaymentRate
-    mmPR = 0                    #minimum Monthly payment
+    ub = balance                    #unpaid balance
+    mir = annualInterestRate / 12.0 #Monthly payment rate
+    fp = 10                         #fixed pay, start with 10
     nub = 0
-    tap = 0                     #Total ammount paid
-    for i in range(12):
-        #Calculate the month payment
-        mmPR = ub * mPR
-        #Calculate the outstanding balance
-        nub = ub - mmPR
-        #print("Month " + str(i+1) + " Remaining balance: " + str(round(nub, 2)))
-        tap += nub
-        #Calculate the new outstandig rate
-        ub = nub + (aIR/12.0)*nub
-    return ub
+    paid = False                    #Is debt paid off?
     
-print("Remainning balance: " + str(round(ccBalance(484, 0.2, 0.04), 2)))
+    #Calculate the minimum monthly payment
+    while not paid:
+        #The original fixed pay works?
+        for i in range(12):
+            #caculate new outsatanding balance, unpaid balance - fixed pay
+            nub = ub - fp
+            #calculate new debt
+            ub = (1 + mir) * nub
+        
+        #Is debt zero?
+        if ub > 0:
+            #No, it is not. Try again with a new fixed pay
+            fp += 10
+            ub = balance
+        else:
+            #yes, it is
+            paid = True
+    
+    #return fixed pay
+    return fp
+    
+fp = fpccBalance(3329, 0.2)
+print ("Lowest payment: " + str(fp))
