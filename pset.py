157932343e3a659feec187ab79562ec2dc6ef7bf
Jose Septien <jaseptienh@gmail.com>
2016-09-16 17:08:52 -0500

    Add problem 3 solution

--- pset.py
+++ pset.py
@@ -9,41 +9,54 @@ Problem 2.
 Write a program that calculates he minimum fixed monthly payment needed in 
 order pay off a credit card balance within 12 months. By a fixed monthly payment, 
 we mean a single number which does not change each month, but instead is a constant 
-amount that will be paid each month.
+amount that will be paid each month. Use bisection search
 """
 
 def fpccBalance(balance, annualInterestRate):
     """
     Calculates the minimum monthly payment that pay off the debt in one year
+    using bisection search.
     balance: int or float. Indicate the oustanding balance of the credit card
     annualInterestRate: float. annual interes rate as decimal.
     """
-    ub = balance                    #unpaid balance
-    mir = annualInterestRate / 12.0 #Monthly payment rate
-    fp = 10                         #fixed pay, start with 10
-    nub = 0
-    paid = False                    #Is debt paid off?
-    
-    #Calculate the minimum monthly payment
-    while not paid:
-        #The original fixed pay works?
+    def calculatefp(mpub, mplb, mir, b):
+        """
+        Calculate the fixed pay recursively.
+        mpub: monthly payment upper bound, decimal.
+        mplb: monthly payment lower bound, decimal.
+        mir: monthly interest rate, decimal.
+        b: balance, decimal
+        """
+        #Calculate fixed pay for given bounds
+        fp = (mpub + mplb) / 2.0
+        ub = b
+        #Calculate balance after 12 months of payments
+        #It must be zero if fp is the correct
         for i in range(12):
-            #caculate new outsatanding balance, unpaid balance - fixed pay
-            nub = ub - fp
-            #calculate new debt
-            ub = (1 + mir) * nub
+            #unpaid balance
+            ub = ub -  fp
+            ub *= (1 + mir)
         
-        #Is debt zero?
-        if ub > 0:
-            #No, it is not. Try again with a new fixed pay
-            fp += 10
-            ub = balance
+        #Is our debt zero?
+        if round(ub, 4) == 0:
+            #it is!
+            return fp
         else:
-            #yes, it is
-            paid = True
+            #No, it's not
+            #Our monthly payment war too big
+            if ub < 0:
+                return calculatefp(fp, mplb, mir, b)
+            #Our monthly payment was too small
+            else:
+                return calculatefp(mpub, fp, mir, b)
+            
     
-    #return fixed pay
-    return fp
+    mir = annualInterestRate / 12.0 #Monthly payment rate
+    #Calculate initial upper bound
+    mpub = (balance * ((1 + mir) ** 12)) / 12.0
+    #Calculate intial lower bound
+    mplb = balance / 12.0
+    return calculatefp(mpub, mplb, mir, balance)    
     
-fp = fpccBalance(3329, 0.2)
-print ("Lowest payment: " + str(fp))
+fp = fpccBalance(999999, 0.18)
+print ("Lowest payment: " + str(round(fp, 2)))
