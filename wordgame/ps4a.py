dbf57ae140f5eca3d22ced60375b208407a5e520
Jose Septien <jaseptienh@gmail.com>
2016-10-05 22:42:59 -0500

    Add solution of problem 5

--- wordgame/ps4a.py
+++ wordgame/ps4a.py
@@ -5,7 +5,7 @@ import string
 
 VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
-HAND_SIZE = 7
+HAND_SIZE = 20
 
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
@@ -282,9 +282,37 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-   
+    option = ""
+    #Initialize hand as empty
+    hand = {}
+    done = False
+    validOption = False
+    #Loop until the game is done
+    while not done:
+        while not validOption:
+            print("Enter n to deal a new hand,", end=" ")
+            print(" r to replay the last hand,", end=" ")
+            option = input("or e to end game: ")
+            if option not in "nre":
+                print("Invalid Comand")
+            elif option == "r" and not hand:
+                print("You have not played a hand yet. Please play a new hand first!")
+                print()
+            else:
+                validOption = True
+       
+        validOption = False
+        #Wich option
+        if option == "n":
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+        elif option == "r":
+            playHand(hand, wordList, HAND_SIZE)
+        else:
+            done = True
+        
+    #Halt
+    return
 
 
 
